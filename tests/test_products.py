39c611035dabb16e8e9cd0de6f09dc5f3654f6da
TatyanaSh <tr3520@bk.ru>
2024-04-05 12:44:40 +0300

    Creation tests

---
+++ tests/test_products.py
@@ -0,0 +1,13 @@
+import pytest
+
+from src.products import Product
+
+@pytest.fixture()
+def product_juice():
+    return Product('Sombrero', 'orange juice', 110, 1000)
+
+def test_init(product_juice):
+    assert product_juice.name == 'Sombrero'
+    assert product_juice.description == 'orange juice'
+    assert product_juice.price == 110
+    assert product_juice.quantity == 1000
