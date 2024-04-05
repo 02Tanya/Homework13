b47ef922bd4a198d6c2ea09eb44bd32cc43daae1
TatyanaSh <tr3520@bk.ru>
2024-04-05 12:42:31 +0300

    Classes separation to 2 files

---
+++ src/categories.py
@@ -0,0 +1,9 @@
+class Category:
+
+    quantity_category = 0
+    quantity_sku = 0
+
+    def __init__(self, name, description, goods):
+        self.name = name
+        self.description = description
+        self.goods = goods
