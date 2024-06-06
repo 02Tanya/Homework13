c855d5ef4f5580edf02cc397045e86634a47caf5
TatyanaSh <tr3520@bk.ru>
2024-06-06 13:26:26 +0300

    Added setter and getter for product's price

--- src/products.py
+++ src/products.py
@@ -12,5 +12,19 @@ class Product:
 
 
     @classmethod
-    def creat_product(cls, name, description, price, quantity):
+    def create_product(cls, name, description, price, quantity):
         return cls(name, description, price, quantity)
+
+
+
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print('Введена некорректная цена!')
+        else:
+            self.__price = new_price
